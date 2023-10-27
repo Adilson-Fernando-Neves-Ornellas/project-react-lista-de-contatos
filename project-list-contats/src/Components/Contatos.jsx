1944ef39cf04ab98f94fbe48c07fda0d52b247fa
Adilson Fernando <ornellas00@hotmail.com>
2023-10-27 00:30:28 -0300

    feat: adicionar ja funcionando

---
+++ project-list-contats/src/Components/Contatos.jsx
@@ -0,0 +1,8 @@
+const Contatos = [{
+  id: 1,
+  nome: "Adilson",
+  telefone: "22981272033",
+  temWhats: Boolean,
+  observacao: "a",
+}];
+export {Contatos} ;
---
+++ project-list-contats/src/Components/MainPagInicial.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import "./MainPagInicial.css";
+import { Contatos } from "./Contatos";
+
+const MainPagInicial = () => {
+  const [contatos, setContatos] = useState(Contatos);
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [temWhats, setTemWhats] = useState(true);
+  const [observacao, setObservacao] = useState("");
+
+  // Função para traduzir o valor booleano em português
+  const traduzirBoolean = (valor) => {
+    return valor ? "Sim" : "Não";
+  };
+
+  const adicionar = (e) => {
+    e.preventDefault();
+
+    const verificandoTelefone = contatos.find((user) => user.telefone === telefone);
+    if (verificandoTelefone) {
+      alert("Esse Telefone já está cadastrado!");
+      return false;
+    }
+
+    let novoContato = {
+      id: (contatos.length + 1),
+      nome: nome,
+      telefone: telefone,
+      temWhats: temWhats,
+      observacao: observacao,
+    };
+
+    setContatos([...contatos, novoContato]);
+
+    setNome("");
+    setTelefone("");
+    setTemWhats(true);
+    setObservacao("");
+  };
+
+  const editar = () => {
+  };
+
+  const deletar = () => {
+  };
+
+
+  return (
+    <>
+      <div className="container">
+        <form className="formCadastro" onSubmit={adicionar}>
+          <h1 className="titulo">Formulário de cadastro:</h1>
+          <div className="divInputs">
+            <label htmlFor="nome" className="form-label">
+              Nome
+            </label>
+            <input
+              value={nome}
+              type="text"
+              className="input"
+              id="nome"
+              placeholder="Nome"
+              required
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </div>
+          <div className="divInputs">
+            <label htmlFor="telefone" className="form-label">
+              Telefone
+            </label>
+            <input
+              value={telefone}
+              type="tel"
+              className="input"
+              id="telefone"
+              placeholder="Telefone"
+              required
+              onChange={(e) => setTelefone(e.target.value)}
+            />
+          </div>
+          <div className="divInputs">
+            <label htmlFor="observacao" className="form-label">
+              Observação
+            </label>
+            <input
+              value={observacao}
+              type="text"
+              className="input"
+              id="observacao"
+              placeholder="Observação"
+              required
+              onChange={(e) => setObservacao(e.target.value)}
+            />
+          </div>
+          <div className="divInputsCheckbox">
+            <label htmlFor="temWhats" className="form-label">
+              Tem Whatsapp?
+            </label>
+            <input
+              type="checkbox"
+              checked={temWhats}
+              className="inputCheckbox"
+              id="temWhats"
+              onChange={() => setTemWhats(!temWhats)}
+            />
+          </div>
+          <div className="erroForm"></div>
+          <button type="submit" className="buttonSubmit">
+            Enviar
+          </button>
+          <ul className="conteinerContatos">
+            <h1 className="tituloconteinerContatos">Meus Contatos</h1>
+            {contatos.map((item) => (
+              <li className="liContatos">
+                <div className="nomeContato">Nome: {item.nome}</div>
+                <div className="telefoneContato">Telefone: {item.telefone}</div>
+                <div className="observacaoContato">Observação: {item.observacao}</div>
+                <div className="temWhatsContato">Tem WhatsApp? {traduzirBoolean(item.temWhats)}</div>
+                <div className="ConteinerButtonsLi">
+                  <button className="buttonsLi" onClick={editar}>EDITAR</button>
+                  <button className="buttonsLi" onClick={deletar}>DELETAR</button>
+                </div>
+                <hr/>
+              </li>
+            ))}
+          </ul>
+        </form>
+      </div>
+
+    </>
+  );
+};
+
+export default MainPagInicial;
+
