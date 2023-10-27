0b3731eb7ad11386d6fe3fc3953298c085f6753d
Adilson Fernando <ornellas00@hotmail.com>
2023-10-27 12:26:10 -0300

    feat: projeto lista de contatos finalizada

--- project-list-contats/src/Components/MainPagInicial.jsx
+++ project-list-contats/src/Components/MainPagInicial.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MainPagInicial.css";
 import { Contatos } from "./Contatos";
 
@@ -8,48 +8,96 @@ const MainPagInicial = () => {
   const [telefone, setTelefone] = useState("");
   const [temWhats, setTemWhats] = useState(true);
   const [observacao, setObservacao] = useState("");
+  const [editado, setEditado] = useState(false)
+  const [contatoEditadoId, setContatoEditadoId] = useState(null);
 
-  // Função para traduzir o valor booleano em português
+  useEffect(() => {
+    console.log(editado); 
+  }, [editado]);
+
   const traduzirBoolean = (valor) => {
     return valor ? "Sim" : "Não";
   };
 
   const adicionar = (e) => {
     e.preventDefault();
-
-    const verificandoTelefone = contatos.find((user) => user.telefone === telefone);
-    if (verificandoTelefone) {
-      alert("Esse Telefone já está cadastrado!");
-      return false;
+    
+    if(editado === false){
+      const verificandoTelefone = contatos.find((user) => user.telefone === telefone);
+      if (verificandoTelefone) {
+        alert("Esse Telefone já está cadastrado!");
+        return false;
+      }
+      let novoContato = {
+        id: (contatos.length + 1),
+        nome: nome,
+        telefone: telefone,
+        temWhats: temWhats,
+        observacao: observacao,
+      };
+      setContatos([...contatos, novoContato]);
+      
+      setNome("");
+      setTelefone("");
+      setTemWhats(true);
+      setObservacao("");
     }
-
-    let novoContato = {
-      id: (contatos.length + 1),
-      nome: nome,
-      telefone: telefone,
-      temWhats: temWhats,
-      observacao: observacao,
-    };
-
-    setContatos([...contatos, novoContato]);
-
-    setNome("");
-    setTelefone("");
-    setTemWhats(true);
-    setObservacao("");
   };
 
-  const editar = () => {
+  const salvarEdicao = (e) => {
+    e.preventDefault();
+
+    if (contatoEditadoId !== null) {
+      const contatoIndex = contatos.findIndex((contato) => contato.id === contatoEditadoId);
+
+      if (contatoIndex !== -1) {
+        const contatoAtualizado = {
+          id: contatoEditadoId,
+          nome: nome,
+          telefone: telefone,
+          temWhats: temWhats,
+          observacao: observacao,
+        };
+
+        const novosContatos = [...contatos];
+        novosContatos[contatoIndex] = contatoAtualizado;
+        setContatos(novosContatos);
+      }
+
+      setNome("");
+      setTelefone("");
+      setTemWhats(true);
+      setObservacao("");
+      setEditado(false);
+      setContatoEditadoId(null);
+    }
   };
 
-  const deletar = () => {
+  const editar = (id) => {
+    setEditado(true);
+    setContatoEditadoId(id);
+    const achandoOItem = contatos.find((user) => user.id === id);
+
+    if (achandoOItem) {
+      setNome(achandoOItem.nome);
+      setTelefone(achandoOItem.telefone);
+      setTemWhats(achandoOItem.temWhats);
+      setObservacao(achandoOItem.observacao);
+    }
+  };
+  
+
+  const deletar = (id) => {
+    const novaListaContatos = contatos.filter((contato) => contato.id !== id);
+  
+    setContatos(novaListaContatos);
   };
 
 
   return (
     <>
       <div className="container">
-        <form className="formCadastro" onSubmit={adicionar}>
+        <form className="formCadastro" onSubmit={editado?salvarEdicao:adicionar}>
           <h1 className="titulo">Formulário de cadastro:</h1>
           <div className="divInputs">
             <label htmlFor="nome" className="form-label">
@@ -107,19 +155,20 @@ const MainPagInicial = () => {
           </div>
           <div className="erroForm"></div>
           <button type="submit" className="buttonSubmit">
-            Enviar
+            {editado?"Salvar Edicão":"Adicionar"}
           </button>
           <ul className="conteinerContatos">
             <h1 className="tituloconteinerContatos">Meus Contatos</h1>
             {contatos.map((item) => (
               <li className="liContatos">
+                <div className="idContato">Numero: {item.id}</div>
                 <div className="nomeContato">Nome: {item.nome}</div>
                 <div className="telefoneContato">Telefone: {item.telefone}</div>
                 <div className="observacaoContato">Observação: {item.observacao}</div>
                 <div className="temWhatsContato">Tem WhatsApp? {traduzirBoolean(item.temWhats)}</div>
                 <div className="ConteinerButtonsLi">
-                  <button className="buttonsLi" onClick={editar}>EDITAR</button>
-                  <button className="buttonsLi" onClick={deletar}>DELETAR</button>
+                  <button className="buttonsLi" onClick={() => editar(item.id)} type="button">EDITAR</button>
+                  <button className="buttonsLi" onClick={() => deletar(item.id)} type="button">DELETAR</button>
                 </div>
                 <hr/>
               </li>
